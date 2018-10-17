7b3d8209c937116338938ebf421690744db6441e
Stephen McMahon <stephen@symbiote.com.au>
2018-10-17 21:06:00 +1100

    Fix(memberExt) return a username for the From user

--- src/Extension/MemberExtension.php
+++ src/Extension/MemberExtension.php
@@ -2,7 +2,9 @@
 
 namespace Symbiote\Notifications\Extension;
 
+use SilverStripe\ORM\ArrayList;
 use SilverStripe\Core\Extension;
+use SilverStripe\View\ArrayData;
 use SilverStripe\Control\Director;
 use SilverStripe\Security\Permission;
 use Symbiote\MemberProfiles\Pages\MemberProfilePage;
@@ -16,8 +18,24 @@ class MemberExtension extends Extension
             $filter,
             ['ToID' => $this->owner->ID]
         );
-        return InternalNotification::get()
-            ->filter($filter)
-            ->limit($limit, $offset);
+
+        $notifications = ArrayList::create();
+
+        foreach (InternalNotification::get()->filter($filter)->limit($limit, $offset) as $intNote) {
+            $notification = ArrayData::create($intNote->toMap());
+            $notification->setField('FromUsername', $intNote->From()->getNotificationUsername());
+            $notifications->push($notification);
+        }
+
+        return $notifications;
+    }
+
+    public function getNotificationUsername()
+    {
+        if ($this->owner->Username) {
+            return $this->owner->Username;
+        }
+
+        return $this->owner->FirstName . ' ' . $this->owner->Surname;
     }
 }
